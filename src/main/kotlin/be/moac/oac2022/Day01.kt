971b73827706de75f0e2abc2726b10c7633a7eb4
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-01 09:18:11 +0100

    Day one

--- src/main/kotlin/be/moac/oac2022/Day01.kt
+++ src/main/kotlin/be/moac/oac2022/Day01.kt
@@ -1,7 +1,7 @@
 package be.moac.oac2022
 
 fun main() {
-    val input: List<Long> = "/day01_input.txt".readLines { it.toLong() }
+    val input: List<String> = "/day01_input.txt".readLines { it }
 
     println("Part one: ${timed { Day01 partOne input }}")
     println("Part two: ${timed { Day01 partTwo input }}")
@@ -9,7 +9,27 @@ fun main() {
 
 object Day01 {
 
-    infix fun partOne(input: List<Long>): Long = 1L
-    infix fun partTwo(input: List<Long>): Long = 1L
+    infix fun partOne(input: List<String>): Long =
+        input.parse().maxOf { it.calories }
+
+
+    infix fun partTwo(input: List<String>): Long =
+        input.parse()
+            .sortedByDescending { it.calories }
+            .take(3)
+            .sumOf { it.calories }
+
+    private fun List<String>.parse() =
+        this.fold(listOf(Elve())) { acc, calories ->
+            when {
+                calories.isBlank() -> acc + Elve()
+                else -> acc.dropLast(1) + acc.last().add(calories.toLong())
+            }
+        }
+
+
+    data class Elve(val calories: Long = 0L) {
+        fun add(calories: Long) = Elve(this.calories + calories)
+    }
 
 }
---
+++ src/test/kotlin/be/moac/oac2022/Day01Test.kt
@@ -0,0 +1,35 @@
+package be.moac.oac2022
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day01Test {
+
+    val input = listOf(
+        "1000",
+        "2000",
+        "3000",
+        ""    ,
+        "4000",
+        ""    ,
+        "5000",
+        "6000",
+        ""    ,
+        "7000",
+        "8000",
+        "9000",
+        ""    ,
+        "10000"
+    )
+
+    @Test
+    fun `find the elve that carries the most callories`() {
+        assertThat(Day01 partOne input).isEqualTo(24000L)
+    }
+
+    @Test
+    fun `find the sum of the calories of the top 3 elves`() {
+        assertThat(Day01 partTwo input).isEqualTo(45000L)
+    }
+}
