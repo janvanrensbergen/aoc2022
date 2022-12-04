a3f50ba51420ba3234ae1495a6fb869230d3c8e2
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-04 09:56:49 +0100

    Day 4

---
+++ src/main/kotlin/be/moac/oac2022/Day04.kt
@@ -0,0 +1,29 @@
+package be.moac.oac2022
+
+fun main() {
+    val input: List<String> = "/day04_input.txt".readLines { it }
+
+    println("Part one: ${timed { Day04 partOne input }}")
+    println("Part two: ${timed { Day04 partTwo input }}")
+}
+
+object Day04 {
+
+    infix fun partOne(input: List<String>): Int =
+        input.map {
+            it.split(",").map { s -> s.split("-").first().toInt() .. s.split("-").last().toInt()  }
+        }.filter { pairs ->
+            pairs.first().all { pairs.last().contains(it) } ||
+            pairs.last().all { pairs.first().contains(it) }
+        }.size
+
+
+    infix fun partTwo(input: List<String>): Int =
+        input.map {
+            it.split(",").map { s -> s.split("-").first().toInt() .. s.split("-").last().toInt()  }
+        }.filter { pairs ->
+            pairs.first().any { pairs.last().contains(it) } ||
+            pairs.last().any { pairs.first().contains(it) }
+        }.size
+}
+
---
+++ src/test/kotlin/be/moac/oac2022/Day04Test.kt
@@ -0,0 +1,24 @@
+package be.moac.oac2022
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class Day04Test {
+
+       @Test
+    fun `find overlapping sections`() {
+        val input = listOf(
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+        )
+
+
+        assertThat(Day04 partOne input).isEqualTo(2)
+        assertThat(Day04 partTwo input).isEqualTo(4)
+    }
+}
