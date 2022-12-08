84b2192092376a3e8dbd5f8c039f9d1bfc5edd97
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-08 13:14:29 +0100

    Day eight - optimizations

--- src/main/kotlin/be/moac/aoc2022/Day08.kt
+++ src/main/kotlin/be/moac/aoc2022/Day08.kt
@@ -3,71 +3,63 @@ package be.moac.aoc2022
 fun main() {
     val input: List<String> = "/day08_input.txt".readLines { it }
 
-    println("Part one: ${timed(10) { Day08 partOne input }}")
-    println("Part two: ${timed(1) { Day08 partTwo input }}")
+    println("Part one: ${timed { Day08 partOne input }}")
+    println("Part two: ${timed { Day08 partTwo input }}")
 }
 
 
 object Day08 {
 
     infix fun partOne(input: List<String>): Int =
-        with(input.field) { this.trees.count { tree -> this.isVisible(tree) } }
+        with(input.field) { this.trees.count { (coordinate, tree) -> this.isVisible(coordinate, tree) } }
 
     infix fun partTwo(input: List<String>): Int =
-        with(input.field) { this.innerTrees.maxOf { tree -> this.calculateScenicScore(tree) } }
+        with(input.field) { this.innerTrees.maxOf { (coordinate, tree) -> this.calculateScenicScore(coordinate, tree) } }
 
 }
 
 private val List<String>.field: TreeField
     get() {
-        val trees = this.flatMapIndexed { y, row ->
-            row.mapIndexed { x, value -> Tree(Coordinate(x, y), value.digitToInt()) }
-        }.toList()
-
+        val trees = this.flatMapIndexed { y, row -> row.mapIndexed { x, value -> Coordinate(x, y) to Tree(value.digitToInt()) } }.toMap()
         return TreeField(trees)
     }
 
 
-private data class TreeField(val trees: List<Tree> = emptyList()) {
-    private val sizeX: Int = trees.maxOf { it.coordinate.x }
-    private val sizeY: Int = trees.maxOf { it.coordinate.y }
+private data class TreeField(val trees: Map<Coordinate, Tree>) {
+    private val sizeX: Int = trees.maxOf { it.key.x }
+    private val sizeY: Int = trees.maxOf { it.key.y }
 
     val innerTrees =
-        trees.filterNot { it.coordinate.x == 0 || it.coordinate.x == sizeX || it.coordinate.y == 0 || it.coordinate.y == sizeY }
+        trees.filterNot { it.key.x == 0 || it.key.x == sizeX || it.key.y == 0 || it.key.y == sizeY }
 
-    fun calculateScenicScore(tree: Tree): Int {
-        val surroundingTrees = trees.filter { it != tree && (it.coordinate.x == tree.coordinate.x || it.coordinate.y == tree.coordinate.y) }
+    fun calculateScenicScore(coordinate: Coordinate, tree: Tree): Int {
+        val left = coordinate.leftTo().firstOrNull { it.second.height >= tree.height }?.let {  coordinate.x - it.first.x } ?: coordinate.x
+        val right = coordinate.rightTo().firstOrNull { it.second.height >= tree.height }?.let {  it.first.x - coordinate.x } ?: (sizeX - coordinate.x)
+        val up = coordinate.upTo().firstOrNull { it.second.height >= tree.height }?.let {  coordinate.y - it.first.y } ?: coordinate.y
+        val down = coordinate.downTo().firstOrNull { it.second.height >= tree.height }?.let {  it.first.y - coordinate.y } ?: (sizeY - coordinate.y)
 
-        val left = surroundingTrees.asSequence().filter { it.coordinate.x < tree.coordinate.x }.sortedByDescending { it.coordinate.x }
-            .takeWhileInclusive { it.height < tree.height }.count()
-        val right = surroundingTrees.asSequence().filter { it.coordinate.x > tree.coordinate.x }.sortedBy { it.coordinate.x }
-            .takeWhileInclusive { it.height < tree.height }.count()
-        val top = surroundingTrees.asSequence().filter { it.coordinate.y < tree.coordinate.y }.sortedByDescending { it.coordinate.y }
-            .takeWhileInclusive { it.height < tree.height }.count()
-        val bottom = surroundingTrees.asSequence().filter { it.coordinate.y > tree.coordinate.y }.sortedBy { it.coordinate.y }
-            .takeWhileInclusive { it.height < tree.height }.count()
-        return left * right * top * bottom
+        return left * right * up * down
     }
 
-    fun isVisible(tree: Tree) =
+    private fun Coordinate.leftTo() = (this.x - 1 downTo 0).asSequence().map { Coordinate(it, this.y) to trees[Coordinate(it, this.y)]!! }
+    private fun Coordinate.rightTo() = (this.x + 1..sizeX).asSequence().map { Coordinate(it, this.y) to trees[Coordinate(it, this.y)]!! }
+    private fun Coordinate.upTo() = (this.y - 1 downTo 0).asSequence().map { Coordinate(this.x, it) to trees[Coordinate(this.x, it)]!! }
+    private fun Coordinate.downTo() = (this.y + 1..sizeY).asSequence().map { Coordinate(this.x, it) to trees[Coordinate(this.x, it)]!! }
+
+    fun isVisible(coordinate: Coordinate, tree: Tree) =
         when {
-            tree.coordinate.x == 0 || tree.coordinate.x == sizeX -> true
-            tree.coordinate.y == 0 || tree.coordinate.y == sizeY -> true
-            else -> tree.isVisibleInternal()
+            coordinate.x == 0 || coordinate.x == sizeX -> true
+            coordinate.y == 0 || coordinate.y == sizeY -> true
+            else -> tree.isVisibleInternal(coordinate)
         }
 
-    private fun Tree.isVisibleInternal(): Boolean {
-        val surroundingTrees = trees.filter { it != this && (it.coordinate.x == this.coordinate.x || it.coordinate.y == this.coordinate.y) }
-        return surroundingTrees.isVisibleRight(this) || surroundingTrees.isVisibleLeft(this) || surroundingTrees.isVisibleTop(this) || surroundingTrees.isVisibleBottom(
-            this
-        )
-    }
+    private fun Tree.isVisibleInternal(coordinate: Coordinate): Boolean =
+        coordinate.leftTo().all { it.second.height < this.height } ||
+                coordinate.downTo().all { it.second.height < this.height } ||
+                coordinate.rightTo().all { it.second.height < this.height } ||
+                coordinate.upTo().all { it.second.height < this.height }
 
-    private fun List<Tree>.isVisibleLeft(tree: Tree) = this.filter { it.coordinate.x < tree.coordinate.x }.all { it.height < tree.height }
-    private fun List<Tree>.isVisibleRight(tree: Tree) = this.filter { it.coordinate.x > tree.coordinate.x }.all { it.height < tree.height }
-    private fun List<Tree>.isVisibleTop(tree: Tree) = this.filter { it.coordinate.y < tree.coordinate.y }.all { it.height < tree.height }
-    private fun List<Tree>.isVisibleBottom(tree: Tree) = this.filter { it.coordinate.y > tree.coordinate.y }.all { it.height < tree.height }
 }
 
 private data class Coordinate(val x: Int, val y: Int)
-private data class Tree(val coordinate: Coordinate, val height: Int)
+private data class Tree(val height: Int)
