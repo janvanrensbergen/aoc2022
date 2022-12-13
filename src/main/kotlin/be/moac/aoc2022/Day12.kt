ff8bf11496381b116ee0561d668151d5fe5cc246
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-13 09:07:35 +0100

    Day 12 - Part one

--- src/main/kotlin/be/moac/aoc2022/Day12.kt
+++ src/main/kotlin/be/moac/aoc2022/Day12.kt
@@ -1,5 +1,7 @@
 package be.moac.aoc2022
 
+import java.util.PriorityQueue
+
 
 fun main() {
     val input: List<String> = "/day12_input.txt".readLines { it }
@@ -11,23 +13,44 @@ fun main() {
 object Day12 {
 
     infix fun partOne(input: List<String>): Long {
-        val heightMap =input.parse()
-        heightMap.neighBoursOf(Point(Coord(1,-1), 'b'.code)).print()
+        val heightMap = input.parse()
 
-        return 1
+        fun path(current: Point,
+                 visited: Set<Coord> = emptySet(),
+                 steps: Map<Coord, Int> = emptyMap(),
+                 queue: PriorityQueue<Pair<Coord, Int>>
+        ): Int {
+            return when {
+                queue.isEmpty() -> steps[heightMap.finish.coord] ?: 0
+                else -> {
+                    val (coord, step) = queue.remove()
+
+                    path(queue = queue, visited = visited + coord, steps = steps, current = current)
+                }
+            }
+        }
+
+        return path(heightMap.start, heightMap.finish).toLong()
     }
+
     infix fun partTwo(input: List<String>): Long = partOne(input)
 }
 
 
 private fun List<String>.parse(): HeightMap {
-    var start = Point(Coord(-1,-1), 0)
-    var end = Point(Coord(-1,-1), 0)
-    val map = this.flatMapIndexed{ y, line->
+    var start = Point(Coord(-1, -1), 0)
+    var end = Point(Coord(-1, -1), 0)
+    val map = this.flatMapIndexed { y, line ->
         line.mapIndexed { x, c ->
-            when(c) {
-                'S' -> {start=Point(Coord(x, -y) ,'a'.code); Point(Coord(x, -y) ,'a'.code)}
-                'E' -> {end=Point(Coord(x, -y), 'z'.code);  Point(Coord(x, -y), 'z'.code)}
+            when (c) {
+                'S' -> {
+                    start = Point(Coord(x, -y), 'a'.code); Point(Coord(x, -y), 'a'.code)
+                }
+
+                'E' -> {
+                    end = Point(Coord(x, -y), 'z'.code); Point(Coord(x, -y), 'z'.code)
+                }
+
                 else -> Point(Coord(x, -y), c.code)
             }
         }
@@ -46,16 +69,18 @@ private data class HeightMap(
     private val left = Coord(-1, 0)
     private val right = Coord(1, 0)
 
-    fun neighBoursOf(point: Point) =
+    fun neighboursOf(point: Point) =
         listOf(left, down, right, up)
             .map { point.coord + it }
             .mapNotNull { map[it] }
-            .filter { point.height <= it.height }
+            .filter { (it.height - point.height) <= 1L }
 
 }
+
 private data class Coord(val x: Int, val y: Int) {
     operator fun plus(coord: Coord) =
         Coord(this.x + coord.x, this.y + coord.y)
 
 }
+
 private data class Point(val coord: Coord, val height: Int)
