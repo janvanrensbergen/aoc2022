a2d9488def17287228d13bbe7385f772aefbc77c
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-13 10:32:53 +0100

    Day 12 - part 2

--- src/main/kotlin/be/moac/aoc2022/Day12.kt
+++ src/main/kotlin/be/moac/aoc2022/Day12.kt
@@ -4,7 +4,7 @@ package be.moac.aoc2022
 fun main() {
     val input: List<String> = "/day12_input.txt".readLines { it }
 
-    println("Part one: ${timed(0) { Day12 partOne input }}")
+//    println("Part one: ${timed(5) { Day12 partOne input }}")
     println("Part two: ${timed(0) { Day12 partTwo input }}")
 }
 
@@ -12,35 +12,38 @@ object Day12 {
 
     infix fun partOne(input: List<String>): Long {
         val heightMap = input.parse()
-
-        fun path(
-            queue: ArrayDeque<Point>,
-            visited: Set<Coord> = emptySet(),
-            steps: Map<Coord, Int> = emptyMap(),
-        ): Int = when {
-            queue.isEmpty() -> steps[heightMap.finish.coord] ?: 0
-            else -> {
-                val newSteps = steps.toMutableMap()
-                val current = queue.removeFirst()
-                val newStep = steps.getOrDefault(current.coord, Int.MAX_VALUE) + 1
-                heightMap.neighboursOf(current)
-                    .filterNot { visited.contains(it.coord) }
-                    .filter { neighbour -> newStep < steps.getOrDefault(neighbour.coord, Int.MAX_VALUE) }
-                    .forEach { neighbour ->
-                        newSteps[neighbour.coord] = newStep
-                        queue.addLast(neighbour)
-                    }
-
-                path(queue = queue, visited = visited + current.coord, steps = newSteps)
-            }
-        }
-
-        return path(queue = ArrayDeque(listOf(heightMap.start)), steps = mapOf(heightMap.start.coord to 0)).toLong()
+        return heightMap.path(queue = ArrayDeque(listOf(heightMap.start)), steps = mapOf(heightMap.start.coord to 0)).toLong()
     }
 
-    infix fun partTwo(input: List<String>): Long = partOne(input)
+    infix fun partTwo(input: List<String>): Long {
+        val heightMap = input.parse()
+        val lowestPoints = heightMap.lowestPoints.print()
+        val paths = lowestPoints.map { point -> heightMap.path(queue = ArrayDeque(listOf(point)), steps = mapOf(point.coord to 0)).toLong() }.print()
+        return paths.filter { it > 0 }.min()
+    }
 }
 
+private fun HeightMap.path(
+    queue: ArrayDeque<Point>,
+    visited: Set<Coord> = emptySet(),
+    steps: Map<Coord, Int> = emptyMap(),
+): Int = when {
+    queue.isEmpty() -> steps[this.finish.coord] ?: 0
+    else -> {
+        val newSteps = steps.toMutableMap()
+        val current = queue.removeFirst()
+        val newStep = steps.getOrDefault(current.coord, Int.MAX_VALUE) + 1
+        this.neighboursOf(current)
+            .filterNot { visited.contains(it.coord) }
+            .filter { neighbour -> newStep < steps.getOrDefault(neighbour.coord, Int.MAX_VALUE) }
+            .forEach { neighbour ->
+                newSteps[neighbour.coord] = newStep
+                queue.addLast(neighbour)
+            }
+
+        path(queue = queue, visited = visited + current.coord, steps = newSteps)
+    }
+}
 
 private fun List<String>.parse(): HeightMap {
     var start = Point(Coord(-1, -1), 0)
@@ -73,6 +76,9 @@ private data class HeightMap(
     private val down = Coord(0, -1)
     private val left = Coord(-1, 0)
     private val right = Coord(1, 0)
+
+    val lowestPoints get() =
+        map.filter { (_, point) -> point.height == 'a'.code } .map { it.value }
 
     fun neighboursOf(point: Point) =
         listOf(left, down, right, up)
