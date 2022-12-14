053cd300830d19f64aaff06b93428b16634a5d9d
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2022-12-14 22:54:46 +0100

    Day 14 - part 1

--- src/main/kotlin/be/moac/aoc2022/Day14.kt
+++ src/main/kotlin/be/moac/aoc2022/Day14.kt
@@ -1,15 +1,84 @@
 package be.moac.aoc2022
 
+import be.moac.aoc2022.Day14.Result.Sand
+
 fun main() {
     val input: List<String> = "/day14_input.txt".readLines { it }
 
-    println("Part one: ${timed { Day14 partOne input }}")
+    println("Part one: ${timed(0) { Day14 partOne input }}")
     println("Part two: ${timed { Day14 partTwo input }}")
 }
 
 object Day14 {
 
-    infix fun partOne(input: List<String>): Long = TODO()
+    infix fun partOne(input: List<String>): Int {
+        val rocks = input.parseRocks()
+        val lowest = rocks.minBy { it.y }.y
+
+        val movements = listOf(Coordinate(0, -1), Coordinate(-1, -1), Coordinate(1, -1))
+
+        tailrec fun drop(current: Coordinate = Coordinate(500, 0),
+                 sand: List<Coordinate> = emptyList()): Result {
+            val next = movements.map { it + current }.filterNot { (rocks + sand).contains(it) }
+            return when {
+                next.isEmpty() -> Sand( sand + current)
+                next.first().y < lowest  -> Result.Done(sand.size)
+                else -> drop(next.first(), sand)
+            }
+        }
+
+        tailrec fun untilTheEndlessVoidComes(result: Result): Result {
+            return when(result) {
+                is Result.Done -> result
+                is Sand -> untilTheEndlessVoidComes(drop(sand = result.value))
+            }
+        }
+
+        return (untilTheEndlessVoidComes(Sand()) as Result.Done).result
+    }
+
     infix fun partTwo(input: List<String>): Long = TODO()
+
+
+    private sealed interface Result {
+
+        data class Sand(val value: List<Coordinate> = emptyList()): Result
+        data class Done(val result: Int): Result
+    }
+
+    private fun print(rocks: List<Coordinate>, sand: List<Coordinate>): String {
+        return (0..9).joinToString(separator = "\n") { y ->
+            (494..503).joinToString(separator = "") { x ->
+                when {
+                    rocks.contains(Coordinate(x, -y)) -> "#"
+                    sand.contains(Coordinate(x, -y)) -> "o"
+                    else -> "."
+                }
+            }
+        }
+    }
+
+    private fun List<String>.parseRocks() =
+        this.flatMap { line ->
+            line.split(" -> ").zipWithNext()
+                .flatMap {
+                    val (firstX, firstY) = it.first.split(",")
+                    val (secondX, secondY) = it.second.split(",")
+                    (firstX.toInt() range secondX.toInt()).flatMap { x ->
+                        (-firstY.toInt() range -secondY.toInt()).map { y -> Coordinate(x, y) }
+                    }
+                }
+        }
+
+    private data class Coordinate(val x: Int, val y: Int) {
+        operator fun plus(other: Coordinate): Coordinate = Coordinate(this.x + other.x, this.y + other.y)
+    }
+
+    private infix fun Int.range(other: Int) =
+        when {
+            this > other -> (this downTo other)
+            else -> (this..other)
+
+        }
 
 }
--- src/test/kotlin/be/moac/aoc2022/Day14Test.kt
+++ src/test/kotlin/be/moac/aoc2022/Day14Test.kt
@@ -1,5 +1,18 @@
 package be.moac.aoc2022
 
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class Day14Test {
     
+    private val input = """
+    498,4 -> 498,6 -> 496,6
+    503,4 -> 502,4 -> 502,9 -> 494,9
+    """.trimIndent().lines()
+
+    @Test
+    fun `part one`() {
+        assertThat(Day14 partOne input).isEqualTo(24)
+    }
 }
